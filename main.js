8993f883f6fde14e9bf0a0651ba83865fdf01852
shishirasonice <82163635+shishirasonice@users.noreply.github.com>
2021-08-13 20:24:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var GuestName = document.getElementById("name1").nodeValue;
+name_of_people.push(GuestName);
+
+document.getElementById("sort_button").style.display="block";
+names_of_people.sort();
